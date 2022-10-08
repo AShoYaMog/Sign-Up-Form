8ea42f7155fa7d0af9dbc2e330e9b0b2e0e90024
AShoYaMog <ivan.litvinenko.personal@gmail.com>
2022-10-08 16:07:21 +0300

    improved display of password mismatches

--- script/validator.js
+++ script/validator.js
@@ -1,6 +1,18 @@
 const PASSWORD = document.querySelector('#password');
-var PASSWORD_CHECK = document.querySelector('#password_check')
+const PASSWORD_CHECK = document.querySelector('#password_check')
 
 PASSWORD_CHECK.onkeyup = () => {
-    PASSWORD.value !== PASSWORD_CHECK.value ? PASSWORD_CHECK.classList.add('error') : PASSWORD_CHECK.classList.remove('error')
+    PASSWORD.value !== PASSWORD_CHECK.value ? addError() : removeError();
 } 
+
+function addError() {
+    PASSWORD.classList.add('error');
+    PASSWORD_CHECK.classList.add('error');
+    document.querySelector('#passwordError').style.display = 'block';
+}
+
+function removeError() {
+    PASSWORD.classList.remove('error');
+    PASSWORD_CHECK.classList.remove('error');
+    document.querySelector('#passwordError').style.display = 'none';
+}
